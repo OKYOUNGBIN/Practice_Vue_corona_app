fe994e213ba80fdace9e8633e700bde589b0a6eb
youngbin <redmangos@naver.com>
2022-04-19 21:41:46 +0900

    domestic

--- src/components/corona/DomesticCases.vue
+++ src/components/corona/DomesticCases.vue
@@ -1,10 +1,19 @@
 <template>
-  <h2>국내현황</h2>
+  <div>
+    <ul class="graph-list">
+      <li>
+        <h3>누적 확진/사망자 추이</h3>
+        <ChartGraph />
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
+import ChartGraph from "../charts/ChartGraph.vue";
 export default {
   name: "DomesticCases",
+  components: { ChartGraph },
 };
 </script>
 
