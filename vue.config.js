bb1ce4ec035d12229128d01ca4dfe442f915f0a8
youngbin <redmangos@naver.com>
2022-04-21 21:34:48 +0900

    build

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,13 @@
 //   transpileDependencies: true,
 // });
 
-module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/vue-corona-app" : "/",
-  outputDir: "docs",
-};
+// module.exports = {
+//   publicPath: process.env.NODE_ENV === "production" ? "/vue-corona-app" : "/",
+//   outputDir: "./docs",
+// };
+const { defineConfig } = require("@vue/cli-service");
+module.exports = defineConfig({
+  transpileDependencies: true,
+  publicPath: "/vue-corona-app/",
+  outputDir: "./docs",
+});
